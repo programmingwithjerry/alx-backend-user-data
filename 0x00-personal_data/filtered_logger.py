f577f0487ec895fdf39d76230263c1a10c02d361
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-07 17:29:30 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -57,10 +57,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
+
     def __init__(self, fields: List[str]):
         """ Initializes the formatter with fields to redact """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
+
 
     def format(self, record: logging.LogRecord) -> str:
         """ Applies redaction to sensitive fields in log records """
