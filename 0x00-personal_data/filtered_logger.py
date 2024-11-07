7d00b9b7a45654dfdaf9a1da0fa5c552e66b5fe4
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-07 11:01:47 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,87 +1,99 @@
 #!/usr/bin/env python3
-"""
-Handling Personal Data
-"""
+""" Module for protecting sensitive information (PII) in log records """
 
-import logging
-import os
-import re
 from typing import List
-import mysql.connector
+import logging
+import re
+from mysql.connector import connection
+from os import environ
+
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
-def filter_datum(fields: List[str], redaction_text: str, log_message: str,
-                 delimiter: str) -> str:
-    """ Replace field values with a redacted string in the log message """
+def filter_datum(fields: List[str], mask: str,
+                 log_message: str, delimiter: str) -> str:
+    """ Replaces specified fields in log_message with a redaction mask """
+    obscured_message = log_message
     for field in fields:
-        log_message = re.sub(rf"{field}=(.*?)\{delimiter}",
-                             f'{field}={redaction_text}{delimiter}', \
-                               log_message)
-    return log_message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Formatter class that redacts sensitive data """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    DELIMITER = ";"
-
-    def __init__(self, fields_to_redact: List[str]):
-        """ Initialize the formatter with the fields to be redacted """
-        self.fields_to_redact = fields_to_redact
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Apply redaction to the formatted log record """
-        return filter_datum(self.fields_to_redact, self.REDACTION,
-                            super().format(record), self.DELIMITER)
+        obscured_message = re.sub(f"{field}=.*?{delimiter}",
+                                  f"{field}={mask}{delimiter}", obscured_message)
+    return obscured_message
 
 
 def get_logger() -> logging.Logger:
-    """ Set up and configure a logger """
-    user_logger = logging.getLogger("user_data")
-    user_logger.setLevel(logging.INFO)
-    user_logger.propagate = False
+    """ Configures and returns a logger for user data """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    user_logger.addHandler(stream_handler)
-    return user_logger
+    logger.addHandler(stream_handler)
+    return logger
 
 
-def get_db_connection() -> mysql.connector.connection.MySQLConnection:
-    """ Establish and return a database connection """
-    db_password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    db_username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
-    db_host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    connection = mysql.connector.connect(
-        host=db_host,
-        database=db_name,
-        user=db_username,
-        password=db_password)
-    return connection
+def get_db() -> connection.MySQLConnection:
+    """
+    Establishes a database connection using environment variables
+    """
+    user = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    database = environ.get("PERSONAL_DATA_DB_NAME")
+    
+    conn = connection.MySQLConnection(
+        user=user,
+        password=password,
+        host=host,
+        database=database
+    )
+    return conn
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Custom logging formatter to redact specified fields """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initializes the formatter with fields to redact """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Applies redaction to sensitive fields in log records """
+        return filter_datum(
+            self.fields, self.REDACTION, super(
+                RedactingFormatter, self).format(record),
+            self.SEPARATOR)
 
 
 def main() -> None:
-    """ Execute the main logic of connecting to the
-        database and retrieving data """
-    db_connection = get_db_connection()
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM users;")
-    for user_record in cursor:
-        log_message = f"name={user_record[0]}; email={user_record[1]}; \
-                        phone={user_record[2]}; " +\
-            f"ssn={user_record[3]}; password={user_record[4]}; \
-               ip={user_record[5]}; " +\
-            f"last_login={user_record[6]}; user_agent={user_record[7]};"
-        print(log_message)
+    """
+    Connects to the database, retrieves user data,
+    and logs each record with sensitive data redacted
+    """
+    db = get_db()
+    cursor = db.cursor()
+
+    query = 'SELECT * FROM users;'
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    logger = get_logger()
+
+    for row in results:
+        log_message = 'name={}; email={}; phone={}; ssn={}; password={}; ip={}; '\
+                      'last_login={}; user_agent={};'
+        log_message = log_message.format(row[0], row[1], row[2], row[3],
+                                         row[4], row[5], row[6], row[7])
+        logger.info(log_message)
+
     cursor.close()
-    db_connection.close()
+    db.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
