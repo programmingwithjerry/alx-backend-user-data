9b8dd7ab41884811f47b832324d7d545999cff3a
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-14 19:10:14 +0100

    updated app.py and user.py

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+from os import getenv
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized error handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    Executes prior to each request to handle authentication checks
+    """
+    # Define a list of routes that do not require authentication
+    open_routes = ['/api/v1/status/',
+                   '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                   '/api/v1/auth_session/login/']
+
+    # Check if the current request path requires authentication
+    if auth and auth.require_auth(request.path, open_routes):
+        """If no Authorization header is present, respond with
+        a 401 Unauthorized"""
+        if not auth.authorization_header(request):
+            abort(401)
+        """If the header is present but session cookie is missing,
+           respond with a 401"""
+        if (auth.authorization_header(request) and
+            not auth.session_cookie(request)):
+            abort(401)
+        # Set the current user for the request if authenticated
+        request.current_user = auth.current_user(request)
+        # If user authentication fails, respond with a 403 Forbidden
+        if not auth.current_user(request):
+            abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module for API authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Handles authentication methods """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is needed for
+            specific API routes """
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Verifies the presence of the Authorization
+            header in the request
+        and checks if it contains a value """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Placeholder method for obtaining the current user """
+        return None
