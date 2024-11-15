4015bf41474a237b527c3159c45d1ff454a4fc39
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-15 20:06:42 +0100

    users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -18,11 +18,11 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """ 
+    """
     GET /api/v1/users/:id
     Path parameter:
       - User ID
-    Returns:
+    Return:
       - JSON representation of the User object
       - 404 error if the specified User ID is not found
     """
