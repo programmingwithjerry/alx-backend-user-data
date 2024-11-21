02aa34ee1c80af77f8b7394c913e8d130ad33525
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-21 18:01:01 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-"""A simple Flask app with a single route that returns a JSON payload.
+"""Flask app for managing user authentication and account features.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, redirect
+
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/", methods=["GET"])
-def home():
-    """Handles GET requests to the root URL and returns a JSON payload."""
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """Handles GET requests to the home route.
+    Returns:
+        - A JSON response with a welcome message.
+    """
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
-    """Handles POST requests to /users for account creation.
+    """Handles POST requests to create a new user.
     Returns:
-        - A JSON response indicating the result of the operation.
+    - A JSON response indicating the success or failure of the operation.
     """
     email, password = request.form.get("email"), request.form.get("password")
     try:
@@ -25,6 +31,78 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """Handles POST requests for user login.
+    Return:
+    - A JSON response confirming login success or aborts if invalid.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """Handles DELETE requests to log out a user.
+    Return:
+        - A redirect to the home route after session destruction.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """Handles GET requests to fetch user profile information.
+    Return:
+    - A JSON response with the user's email or aborts if unauthorized.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Handles POST requests to generate a password reset token.
+    Return:
+    - A JSON response with the reset token or aborts if the user doesn't exist.
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """Handles PUT requests to update a user's password.
+    Return:
+     - A JSON response confirming the password change or aborts if invalid.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
